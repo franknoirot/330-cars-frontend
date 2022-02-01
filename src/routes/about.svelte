b6c0273b1c3da24bf2eacd4989cc64c351a40e00
Frank Johnson <frankjohnson1993@gmail.com>
2022-02-01 18:46:16 -0500

    Add initial pages

---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<h1>About 330 Cars</h1>
+<p>
+    We're locally-owned and operated right here in the 330. 
+    We'll get you in the car you need so you get there and back in style.
+</p>
+<h2>Ohio Proud</h2>
+<p>
+    We are 3rd and 4th generation Ohioans with deep roots in Akron. 
+    We will make sure you have a good experience renting from us because 
+    we know if not our mothers (and grandmas) will call us up about it.
+</p>
+<h2>Her Story</h2>
+<p>
+    Our founder, Dani Johnson, is Northeast Ohio’s only female car dealership owner. 
+    After successfully launching and running Her Cars in Akron for 5 years, she founded 330 Cars 
+    to meet the needs of people in the community who only need a car occasionally.
+</p>
---
+++ src/routes/help.svelte
@@ -0,0 +1,52 @@
+<script context="module">
+    export async function load() {
+        // TODO: Create FAQ content type in Studio
+        const faqs = [
+            {
+                question: "How soon before my reservation can I cancel?",
+                answer: "You can cancel up to 2 hours before your scheduled pickup time.",
+            },
+            {
+                question: "Can I take a car?",
+                answer: "You may *buy* any rental car we have.",
+            },
+            {
+                question: "What color are your cars?",
+                answer: "What do you mean? They're all sorts of colors, I mean cars come in different colors—what are you talking about?",
+            },
+        ]
+
+        return {
+            props: { faqs },
+        }
+    }
+</script>
+
+<script>
+    export let faqs = []
+</script>
+
+<h1>Help & Rental FAQs</h1>
+<p>
+    We know rentals can be tricky to trust. There is a lot of fine print in those little contracts, 
+    so as a small local business we want to make sure you're as comfortable as possible in choosing 
+    us for your transportation needs.
+</p>
+<section>
+    {#each faqs as faq, i ('faq-'+i)}
+    <details>
+        <summary>{ faq.question }</summary>
+        { faq.answer }
+    </details>
+    {/each}
+</section>
+
+<style>
+    details {
+        margin: 2rem 0;
+    }
+
+    summary {
+        margin-bottom: 1rem;    
+    }
+</style>
