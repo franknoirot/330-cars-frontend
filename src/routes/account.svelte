4ef10b4c1aa8cdd468dd5b34d20c7ca360673425
Frank Johnson <frankjohnson1993@gmail.com>
2022-03-16 23:32:19 -0400

    Debugging account load

--- src/routes/account.svelte
+++ src/routes/account.svelte
@@ -1,11 +1,9 @@
 <script context="module">
-    import { userStore } from '$app/stores'
     import { getCustomerById } from '$lib/sanity'
     let customer = {}
 
-    /** @type {import('./[slug]').Load} */
     export async function load({ session }) {
-        const user = userStore.get() || session
+        const user = session?.user
         console.log({ session })
         if (!user) {
             return {
