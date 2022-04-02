4c1c883a3f718a8acf5f934fd5c6491b5c84e39a
Frank Johnson <frankjohnson1993@gmail.com>
2022-04-02 17:09:55 -0400

    Add basic 404

---
+++ src/routes/__error.svelte
@@ -0,0 +1,3 @@
+<h1>404: page not found</h1>
+<p>Sorry, it looks like that page doesn't exist!</p>
+<p>This site is still under development, so there's a good chance you've found an incomplete part of it. Follow development progress on <a href="https://github.com/franknoirot/330-cars-frontend" target="_blank" rel="noreferrer nofollower">GitHub</a> in the meantime.</p>
